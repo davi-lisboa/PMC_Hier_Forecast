fd92506f9815d5bf179501b3e9fec387e29a2058
Davi Lisboa <lisboa.davi1703@gmail.com>
2026-03-16 15:33:14 -0300

    Lotta' tiny things
    * Avanços no processo de coleta do número índice e dos pesos
    
    * Construção inicial do model a ser utilizado
    
    * Persistencia dos dados, etc

--- pipeline/coleta.py
+++ pipeline/coleta.py
@@ -12,7 +12,7 @@ from typing import Union, List, Literal, LiteralString
 
 @retry(stop=stop_after_attempt(5), wait=wait_fixed(2))
 def get_pmc_index(
-                tipo: str=Literal['ampliada', 'restrita']
+                tipo: str=Literal['ampliada', 'ampliada_sem_aberturas', 'restrita', 'restrita_sem_aberturas']
                 
                 ) -> pd.DataFrame:
     
@@ -20,9 +20,56 @@ def get_pmc_index(
     # ampliada_url: https://apisidra.ibge.gov.br/values/t/8883/n1/all/v/7169/p/all/c11046/56736/c85/all/d/v7169%205
     meta = {
         'restrita': {'table_code': '8882', 'classification':'11046/56734/c85/all'},
+        'restrita_sem_aberturas': {'table_code': '8882', 'classification': '11046/56734/c85/2759,90671,90672,90673,103155,103156,103157,103158'},
+        
         'ampliada': {'table_code': '8883', 'classification':'11046/56736/c85/all'},
+        'ampliada_sem_aberturas': {'table_code': '8883', 'classification':'11046/56736/c85/2759,2762,56741,90671,90672,90673,103155,103156,103157,103158,103159'}
     }
 
+    pmc_hier_dict = {
+    'Combustíveis e lubrificantes': 
+        '1. Combustíveis e lubrificantes',
+
+    'Hipermercados, supermercados, produtos alimentícios, bebidas e fumo':
+        '2. Hipermercados, supermercados, produtos alimentícios, bebidas e fumo',
+
+    'Hipermercados e supermercados':
+        '2.1 Hipermercados e supermercados',
+
+    'Tecidos, vestuário e calçados':
+        '3. Tecidos, vestuário e calçados',
+
+    'Móveis e eletrodomésticos':
+        '4. Móveis e eletrodomésticos',
+
+    'Móveis':
+        '4.1 Móveis',
+
+    'Eletrodomésticos':
+        '4.2 Eletrodomésticos',
+
+    'Artigos farmacêuticos, médicos, ortopédicos, de perfumaria e cosméticos':
+        '5. Artigos farmacêuticos, médicos, ortopédicos, de perfumaria e cosméticos',
+
+    'Livros, jornais, revistas e papelaria':
+        '6. Livros, jornais, revistas e papelaria',
+
+    'Equipamentos e materiais para escritório, informática e comunicação':
+        '7. Equipamentos e materiais para escritório, informática e comunicação',
+
+    'Outros artigos de uso pessoal e doméstico':
+        '8. Outros artigos de uso pessoal e doméstico',
+
+    'Veículos, motocicletas, partes e peças':
+        '9. Veículos, motocicletas, partes e peças',
+
+    'Material de construção':
+        '10. Material de construção',
+
+    'Atacado especializado em produtos alimentícios, bebidas e fumo':
+        '11. Atacado especializado em produtos alimentícios, bebidas e fumo'
+    }
+    
     pmc_index = (
                     sidra.get_table(
                                 table_code= meta[tipo]['table_code'],
@@ -37,14 +84,14 @@ def get_pmc_index(
                 [['Mês (Código)', 'Atividades', 'Valor']]
                 .assign(
                     Data = lambda df: pd.to_datetime(df['Mês (Código)'], format='%Y%m'),
-                    nindice = lambda df: pd.to_numeric(df['Valor'], errors='coerce')
+                    nindice = lambda df: pd.to_numeric(df['Valor'], errors='coerce'),
+                    Atividades = lambda df: df['Atividades'].map(pmc_hier_dict)
                 )
                 .groupby(['Atividades', 'Data'])[['nindice']].last()
     )
     return pmc_index
-    # return meta
 
-get_pmc_index('restrita').index.get_level_values('Atividades').unique()
+# get_pmc_index('restrita_sem_aberturas')
 
 # %% corrige_col_sidra
 
@@ -55,3 +102,39 @@ def corrige_col_sidra(df_sidra: pd.DataFrame) -> pd.DataFrame:
     df_fmt = df_fmt.iloc[1:].sort_index(ignore_index=True)
 
     return df_fmt
+
+# %% get_pmc_pesos
+
+@retry(stop=stop_after_attempt(5), wait=wait_fixed(2))
+def get_pmc_pesos(
+        tipo: str=Literal['ampliada', 'ampliada_sem_aberturas', 'restrita', 'restrita_sem_aberturas']
+            ) -> pd.DataFrame:
+
+    path = r'../data/2023_Participações das Atividades PMC_ Pesos_volume base 2022.xlsx'
+
+    pesos = pd.read_excel(path, sheet_name='Atividades', skipfooter=1)
+
+    match tipo:
+        case 'ampliada_sem_aberturas':
+            pesos = pesos[['Atividades', 'Pesos_vol_varejo_ampliado (Base 2022)']]
+            pesos.columns = ['Atividades', 'Pesos']
+
+        case 'restrita_sem_aberturas':
+            pesos = pesos[['Atividades', 'Pesos_vol_varejo (Base 2022)']]
+            pesos.columns = ['Atividades', 'Pesos']
+            pesos.dropna(inplace=True)
+
+        case 'ampliada': 
+            raise NotImplementedError
+        
+        case 'restrita':
+            raise NotImplementedError
+        
+
+    pesos['Pesos'] = pesos['Pesos'] * 100 
+
+    
+    return pesos
+
+
+# get_pmc_pesos('restrita_sem_aberturas')
---
+++ pipeline/modelo.py
@@ -0,0 +1,100 @@
+# %% Bibliotecas base
+import pandas as pd
+import numpy as np
+import datetime as dt
+
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-v0_8-darkgrid')
+
+# %% Módulos skitme
+
+# ==========
+#  Pipeline
+# ==========
+from sktime.forecasting.compose import TransformedTargetForecaster, ForecastingPipeline
+
+# =============================
+#  Modelos univariados básicos
+# =============================
+from sktime.forecasting.statsforecast import StatsForecastAutoARIMA, StatsForecastAutoETS, StatsForecastAutoCES, StatsForecastAutoTBATS
+
+# ====================
+#  Modelos compostos
+# ====================
+from sktime.forecasting.compose import AutoEnsembleForecaster
+
+# ===============
+#  Regressors
+# ===============
+from lightgbm import LGBMRegressor, DaskLGBMRegressor
+from catboost import CatBoostRegressor
+
+# ============================
+#  Métodos de reconciliação
+# ============================
+from sktime.forecasting.reconcile import BottomUpReconciler, TopdownReconciler, OptimalReconciler, ReconcilerForecaster
+
+# ===============
+#  Transformers
+# ===============
+from sktime.transformations.series.boxcox import LogTransformer, BoxCoxTransformer
+from sktime.transformations.series.detrend import Detrender, Deseasonalizer, ConditionalDeseasonalizer
+from sktime.transformations.series.difference import Differencer
+from sktime.transformations.compose import OptionalPassthrough
+
+# ==================
+#  Cross Validation
+# ==================
+from sktime.split import TemporalTrainTestSplitter, ExpandingWindowSplitter, SlidingWindowSplitter
+from sktime.forecasting.model_evaluation import evaluate
+from sktime.forecasting.model_selection import ForecastingOptunaSearchCV, ForecastingRandomizedSearchCV
+from optuna.distributions import CategoricalDistribution, FloatDistribution, IntDistribution
+
+# ===========
+#  Métricas
+# ===========
+from sktime.performance_metrics.forecasting import MeanAbsoluteError, MeanAbsoluteScaledError
+
+# %%
+
+arima = StatsForecastAutoARIMA(sp=12),
+ets = StatsForecastAutoETS(season_length=12),
+ces = StatsForecastAutoCES(season_length=12),
+tbats = StatsForecastAutoTBATS(seasonal_periods=12)
+lgbm  = LGBMRegressor(verbosity=-1)
+catboost = CatBoostRegressor()
+
+pipe = TransformedTargetForecaster(steps=[
+    ('deseason', OptionalPassthrough(Deseasonalizer(sp=12))),
+    ('detrend', OptionalPassthrough(Detrender())),
+    ('forecaster', AutoEnsembleForecaster(
+        forecasters = [ arima, ets, ces, tbats], 
+        regressor = lgbm
+    )),
+    ('reconciler', OptimalReconciler())
+
+])
+
+param_grid = {
+    'deseason_passthrough': CatBoostRegressor((True, False)),
+    'deseason__transformer__model': CategoricalDistribution(('additive', 'multiplicative')),
+    'forecaster__regressor': CategoricalDistribution((lgbm, catboost)),
+    'reconciler': CategoricalDistribution((BottomUpReconciler(), TopdownReconciler(), OptimalReconciler()))
+}
+
+
+cv = ExpandingWindowSplitter(fh=range(1, 24+1), initial_window=120, step_length=1)
+cv = TemporalTrainTestSplitter(test_size=24)
+
+opcv = ForecastingOptunaSearchCV(
+    forecaster=pipe,
+    cv=cv,
+    param_grid=param_grid,
+    scoring=MeanAbsoluteError(),
+    n_evals=10,
+
+)
+
+import joblib
+pmc_agg = joblib.load(r'../data/pmc_agg.joblib')
+opcv.fit(pmc_agg)
